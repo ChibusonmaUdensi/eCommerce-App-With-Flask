ee6672977cc8a542023ae01d21800b8d2d59191c
Mercy <sheroudensi@gmail.com>
2024-09-18 00:28:22 +0100

    eCommerce App Using Flask

--- website/admin.py
+++ website/admin.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, send_from_directory, flash, redirect
 from flask_login import login_required, current_user
 from .forms import ShopItemsForm
 from werkzeug.utils import secure_filename
 admin= Blueprint('admin', __name__)
 from .models import Product
 from . import db
+
+@admin.route('/media/<path:filename>')
+def get_image(filename):
+    return send_from_directory('../media', filename)
+
 
 @admin.route('/add-shop-items', methods=['GET', 'POST'])
 @login_required
@@ -54,4 +59,60 @@ def shop_items():
     if current_user.id == 6:
         items = Product.query.order_by(Product.date_added).all()
         return render_template('shop_items.html', items=items)
+    return render_template('404.html')
+
+@admin.route('/update-item/<int:item_id>', methods=['GET','POST'])
+@login_required
+def update_item(item_id):
+    if current_user.id == 6:
+        item_to_update = Product.query.get(item_id)
+        form = ShopItemsForm()
+      
+        form.product_name.render_kw = {'placeholder': item_to_update.product_name}
+        form.previous_price.render_kw = {'placeholder': item_to_update.previous_price}
+        form.current_price.render_kw = {'placeholder': item_to_update.current_price}
+        form.in_stock.render_kw = {'placeholder': item_to_update.in_stock}
+        form.flash_sale.render_kw = {'placeholder': item_to_update.flash_sale}
+        if form.validate_on_submit():
+            product_name = form.product_name.data
+            current_price = form.current_price.data
+            previous_price = form.previous_price.data
+            in_stock = form.in_stock.data
+            flash_sale = form.flash_sale.data
+
+            file = form.product_picture.data
+            file_name = secure_filename(file.filename)
+            filepath = f'./media/{file_name}'
+
+            file.save(filepath)
+            try:
+                Product.query.filter_by(id=item_id).update(dict(product_name=product_name,
+                                                                current_price=current_price,
+                                                                previous_price=previous_price,
+                                                                in_stock=in_stock,
+                                                                flash_sale=flash_sale,
+                                                                product_picture=filepath))
+                db.session.commit()
+                flash(f'{product_name} updated successfully')
+                return redirect('/shop-items')
+            except Exception as e:
+                print('Product not Updated')
+                flash(f'Item not Updated!!1')
+        return render_template('update_item.html', form=form) 
+    return render_template('404.html')
+
+@admin.route('/delete-item/<int:item_id>', methods=['GET', 'POST'])
+@login_required
+def delete_item(item_id):
+    if current_user.id == 6:
+        try:
+            item_to_delete = Product.query.get(item_id)
+            db.session.delete(item_to_delete)
+            db.session.commit()
+            flash(f'{item_to_delete.product_name} deleted successfully')
+            return redirect('/shop-items')
+        except Exception as e:
+            print('Product not deleted', e)
+            flash(f'Item not deleted!!')
+        return redirect('/shop-items')
     return render_template('404.html')
--- website/views.py
+++ website/views.py
@@ -1,10 +1,57 @@
-from flask import Blueprint, render_template, flash
+from flask import Blueprint, render_template, flash, redirect, request
 from . import db 
 from flask_login import login_user, login_required, logout_user, current_user
-
-
+from .models import Product, Cart
+from . import db
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template('home.html')
+
+    items= Product.query.filter_by(flash_sale=True)
+
+    return render_template('home.html', items=items, cart=Cart.query.filter_by(customer_link=current_user.id).all()
+                            if current_user.is_authenticated else [])
+
+@views.route('/add-to-cart/<int:item_id>')
+@login_required
+def add_to_cart(item_id):
+    item_to_add = Product.query.get(item_id)
+    item_exists = Cart.query.filter_by(product_link=item_id, customer_link=current_user.id).first()
+    
+    if item_exists:
+        try:
+            item_exists.quantity += 1
+            db.session.commit()
+            flash(f'Quantity of {item_exists.product.product_name} has been updated')
+            return redirect(request.referrer)
+        except Exception as e:
+            print('Quantity not Updated', e)
+            flash(f'Quantity of { item_exists.product.product_name } not updated')
+            return redirect(request.referrer)
+    
+
+    new_cart_item = Cart()
+    new_cart_item.quantity = 1
+    new_cart_item.product_link = item_to_add.id
+    new_cart_item.customer_link = current_user.id
+
+    try:
+        db.session.add(new_cart_item)
+        db.session.commit()
+        flash(f'{new_cart_item.product.product_name} has been added to your cart')
+        return redirect(request.referrer)
+    except Exception as e:
+        print('Item not added to cart', e)
+        flash(f'Item {new_cart_item.product.product_name} not added to cart')
+        return redirect(request.referrer)
+    
+@views.route('/cart')
+@login_required
+def show_cart():
+      cart = Cart.query.filter_by(customer_link=current_user.id).all() 
+      amount = 0
+      for item in cart:
+          amount += item.product.current_price * item.quantity
+
+      return render_template('cart.html', cart=cart, amount=amount, total = amount + 200)
