2d229d8c879c7a044712f57b9a19f3fa9f775711
Mercy <sheroudensi@gmail.com>
2024-11-12 03:29:14 +0100

    eCommerce app using flask

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from website import create_app, db
-
+from flask import Flask, jsonify
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+@app.route('/')
+def index():
+    return jsonify({"message": "Hello, this is my Flask app running on Vercel!"})
+
+# This function allows Vercel to access your Flask app
+def handler(request):
+    return app(request.environ, request.start_response)
